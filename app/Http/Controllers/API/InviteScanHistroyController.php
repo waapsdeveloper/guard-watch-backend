3a89662e6300e9dfaa2156477aa941332c0f6abd
Waapsdeveloper <waapsdeveloper@gmail.com>
2024-01-05 15:05:09 +0500

    asfsaf

--- app/Http/Controllers/API/InviteScanHistroyController.php
+++ app/Http/Controllers/API/InviteScanHistroyController.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace App\Http\API\Controllers;
+namespace App\Http\Controllers\API;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
