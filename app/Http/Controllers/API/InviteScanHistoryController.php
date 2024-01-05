bf3bee3e0cecf6115d9608c2c5fe19bfc00884e6
Waapsdeveloper <waapsdeveloper@gmail.com>
2024-01-05 15:20:16 +0500

    sgdg

---
+++ app/Http/Controllers/API/InviteScanHistoryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+// use App\Services\InviteService;
+// use App\Helpers\Helper;
+
+class InviteScanHistoryController extends Controller
+{
+
+    public function add(Request $request)
+    {
+      dd("value");
+    }
+
+}
