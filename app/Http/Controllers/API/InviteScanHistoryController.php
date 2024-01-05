04a006be193cf1ca43b7f9f78e67c682550abb5e
Waapsdeveloper <waapsdeveloper@gmail.com>
2024-01-05 15:51:37 +0500

    safsa

--- app/Http/Controllers/API/InviteScanHistoryController.php
+++ app/Http/Controllers/API/InviteScanHistoryController.php
@@ -75,7 +75,7 @@ class InviteScanHistoryController extends Controller
         }
 
         //
-        $res = $this->service->edit($data);
+        $res = $this->edit($data);
 
         if($res['bool'] == false){
             return self::failure($res['message'], $res);
