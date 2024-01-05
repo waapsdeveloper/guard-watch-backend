91857ad810cc1460eac952881f81737ec7233d5f
Waapsdeveloper <waapsdeveloper@gmail.com>
2024-01-05 15:27:10 +0500

    fassfs

--- app/Http/Controllers/API/InviteScanHistoryController.php
+++ app/Http/Controllers/API/InviteScanHistoryController.php
@@ -13,7 +13,21 @@ class InviteScanHistoryController extends Controller
 
     public function add(Request $request)
     {
-      dd("value");
+        $data =$request->all();
+
+        $validation = Validator::make($data,[
+            'invite_id' => 'required|int|exists:invites,id',
+            'invite_contact_id' => 'required|int|exists:invitecontacts,id',
+            'scan_by_user_id' => 'required',
+            'scan_date_time' => 'required|datetime',
+            'status' => 'required|string'
+
+
+        ]);
+        return ($validation);
+        // if ($validation->fails()) {
+        //     return self::failure($validation->errors()->first());
+        // }
     }
 
 }
