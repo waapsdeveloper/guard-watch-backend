b9541f774c65ee93e5d365c0c90bcba7041b4c69
Waapsdeveloper <waapsdeveloper@gmail.com>
2024-01-05 13:03:32 +0500

    fsd

--- app/Http/Controllers/API/InviteScanHistoryController.php
+++ app/Http/Controllers/API/InviteScanHistoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Services\InviteScanHistoryService;
 use Illuminate\Support\Facades\Validator;
 
 
