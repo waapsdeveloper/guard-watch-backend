9a1fc776533050087f18b441cfe30a54569a22c7
Waapsdeveloper <waapsdeveloper@gmail.com>
2024-01-05 17:06:41 +0500

    dada

--- app/Services/InviteScanHistoryService.php
+++ app/Services/InviteScanHistoryService.php
@@ -32,7 +32,7 @@ class InviteScanHistoryService {
 
     public function list($data){
         $user = Auth::user();
-        $invites = Invite::where(['created_by' => $user->id])->get();
+        $invites = Invite::where(['invite_id' => $invite->id])->get();
         $list = new InviteScanHistoryCollection($invites);
         return ServiceResponse::success('Invite List', $list);
     }
