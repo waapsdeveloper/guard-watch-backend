191fac797fd58f4ff929ce32f3543f4c129ae91b
Waapsdeveloper <waapsdeveloper@gmail.com>
2024-01-05 15:50:21 +0500

    dsga

--- app/Http/Controllers/API/InviteScanHistoryController.php
+++ app/Http/Controllers/API/InviteScanHistoryController.php
@@ -13,20 +13,20 @@ class InviteScanHistoryController extends Controller
 {
 
 
-    public function list(Request $request){
+    // public function list(Request $request){
 
-        $data = $request->all();
+    //     $data = $request->all();
 
-        //
-        $res = $data;
+    //     //
+    //     $res = $data;
 
-        if($res == false){
-            return self::failure($res ,'message', $res);
-        }
+    //     if($res['bool'] == false){
+    //         return self::failure($res['message'], $res);
+    //     }
 
-        return self::success("", $res);
+    //     return self::success("", $res);
 
-    }
+    // }
 
 
     public function add(Request $request)
@@ -55,4 +55,33 @@ class InviteScanHistoryController extends Controller
         return self::success("Test Result", $res);
     }
 
+
+
+
+    public function edit(Request $request, $id){
+
+        $data = $request->all();
+        $data['id'] = $id;
+
+        // validating the required fields
+        $validation = Validator::make($data, [
+            'id' => 'required|exists:invites,id',
+
+        ]);
+
+        // if validation failed
+        if ($validation->fails()) {
+            return self::failure($validation->errors()->first());
+        }
+
+        //
+        $res = $this->service->edit($data);
+
+        if($res['bool'] == false){
+            return self::failure($res['message'], $res);
+        }
+
+        return self::success("Test Result", $res);
+
+    }
 }
