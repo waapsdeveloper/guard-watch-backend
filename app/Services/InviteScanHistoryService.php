2a9e5664d99fead9410e45e4d3e8cc004c330185
Waapsdeveloper <waapsdeveloper@gmail.com>
2024-01-08 12:32:41 +0500

    asfs

--- app/Services/InviteScanHistoryService.php
+++ app/Services/InviteScanHistoryService.php
@@ -36,7 +36,7 @@ class InviteScanHistoryService {
 
     public function list($data){
         $user = Auth::user();
-        $scanHistory = InviteScanHistories::where(['id' => $user->id])->get();
+        $scanHistory = InviteScanHistories::where(['id' => $invite->id])->get();
         dd($scanHistory);
         $list = new InviteScanHistoryCollection($scanHistory);
         return ServiceResponse::success('Invite scan List', $list);
