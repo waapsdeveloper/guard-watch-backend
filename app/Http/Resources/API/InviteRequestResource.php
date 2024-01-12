3ca8fbc8e7eaa913d3d76fa25d4ab8f077e3aaaf
Waapsdeveloper <waapsdeveloper@gmail.com>
2024-01-12 15:22:28 +0500

    ad

--- app/Http/Resources/API/InviteRequestResource.php
+++ app/Http/Resources/API/InviteRequestResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\API;
 
 use Illuminate\Http\Request;
+use App\Models\Space;
 use App\Http\Resources\API\SpaceResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
