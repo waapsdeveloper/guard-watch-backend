36e263992d13e97311144e6f1316fc6570892f0d
abdullah hanif <99804964+waapsdeveloper@users.noreply.github.com>
2024-03-01 17:39:07 +0500

    wewfwwefwe

--- app/Http/Resources/API/SpaceResource.php
+++ app/Http/Resources/API/SpaceResource.php
@@ -24,7 +24,6 @@ class SpaceResource extends JsonResource
             "id" => $obj->id,
             "title" => $obj->title,
             "description" => $obj->description,
-            "name" => $obj->name,
             "type" => $obj->type,
             "address" => $obj->address,
         ];
