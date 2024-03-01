f237c06934f755b8d6d89008c01f514288c016db
Waapsdeveloper <waapsdeveloper@gmail.com>
2024-03-01 16:08:37 +0500

    check

--- app/Models/PhoneNumber.php
+++ app/Models/PhoneNumber.php
@@ -10,4 +10,9 @@ class PhoneNumber extends Model
     use HasFactory;
 
     protected $guarded = [];
+    protected $fillable = [
+        'device_id',
+        'phone_number',
+        'dial_code'
+    ];
 }
