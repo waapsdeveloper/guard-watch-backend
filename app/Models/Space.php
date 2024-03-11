d5c344765fa4b1401a76afce836f07e463967fb5
Waapsdeveloper <waapsdeveloper@gmail.com>
2024-03-11 18:19:31 +0500

    models

--- app/Models/Space.php
+++ app/Models/Space.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Spatial;
 
 class Space extends Model
 {
     use HasFactory;
+    use Spatial;
 
     protected $fillable = [
         'created_by',
